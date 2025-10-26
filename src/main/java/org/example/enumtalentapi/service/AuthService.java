c22e021091379c93b16424377faa4b7416b74ad2
gazar <kolajokimzee2020@gmail.com>
2025-10-26 23:22:15 +0100

    all new update to repo

---
+++ src/main/java/org/example/enumtalentapi/service/AuthService.java
@@ -0,0 +1,16 @@
+package org.example.enumtalentapi.service;
+
+import org.example.enumtalentapi.dto.LoginRequest;
+import org.example.enumtalentapi.dto.SignupRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AuthService {
+    String signup(SignupRequest request);
+    String login(LoginRequest request);
+    String verifyEmail(String tokenStr);
+
+    String logout(String userId);
+
+    String logoutWithToken(String token);
+}
